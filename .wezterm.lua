423a0a47ccdeb207c71de4943ad1eeff7fc2f644
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-09-23 22:49:00 +0900

    add wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'HackGenNerd Console',
+  color_scheme = 'Gruvbox dark, medium (base16)',
+  font_size = 15,
+  default_cursor_style = 'BlinkingBar',
+}
